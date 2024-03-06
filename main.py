427b810d2915a78e4470c7a7496f9e2fc801b515
lasha_shala <nikolaishvililashan1@gmail.com>
2024-03-06 01:35:48 -0800

    threading rewrite with concurrent futures

--- main.py
+++ main.py
@@ -11,38 +11,29 @@ def write_to_json_file(products_list):
         products_json.write(json_object)
 
 
-def fetch_product(products_list, index):
+def fetch_product(index, products_list):
     r = requests.get(f'https://dummyjson.com/products/{index}')
 
     if r.status_code == 200:
         products_list.append(r.json())
     else:
-        print('ERROR: Incorrect status code!')
+        print(f'ERROR: Incorrect status code! Status Code: {r.status_code}')
 
 
 def run_threads(products_list):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for i in range(0, 100):
-            executor.submit(fetch_product, products_list, i+1)
-
-    # for i in range(0, 100):
-    #     thread_pool.append(threading.Thread(target=fetch_product, args=(products_list, i+1)))
-    #     thread_pool[i].start()
-    #
-    # for i in range(0, 100):
-    #     thread_pool[i].join()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        executor.map(lambda index: fetch_product(index, products_list), range(1, 101))
 
 
 def main():
     start = time.perf_counter()
-    # threads = []
     products = []
 
     run_threads(products)
     write_to_json_file(products)
 
     end = time.perf_counter()
-    print(f'Time taken in seconds: {end - start}.')
+    print(f'Time taken in seconds: {end - start}')
 
 
 if __name__ == '__main__':
